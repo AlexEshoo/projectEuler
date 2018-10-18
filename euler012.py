ea20e26c75b19170f29349394f4424a13f43b83e
Alex Eshoo <AlexEshoo@gmail.com>
2018-10-18 11:30:18 -0400

    MADE FILE PYTHON3 COMPATIBLE

--- euler012.py
+++ euler012.py
@@ -4,21 +4,21 @@ factors = 0
 n = 2
 
 while True:
-	num = triangleNumber(n)
+    num = triangleNumber(n)
 
-	if str(num)[-1] != '0' or num % 12 != 0:
-		n += 1
-		continue
-	
-	factors = proper_divisors(num)
-	
-	if len(factors) > 500:
-		break
-	
-	
-	if len(factors) > 200: print n, num, len(factors)
-	n += 1
+    if str(num)[-1] != '0' or num % 12 != 0:
+        n += 1
+        continue
 
-print n, num, len(factors)
+    factors = proper_divisors(num)
 
-### Answer is 76576500 (12375th triangle number, 576 factors)
+    if len(factors) > 500:
+        break
+
+    if len(factors) > 200:
+        print(n, num, len(factors))
+    n += 1
+
+print(n, num, len(factors))
+
+# Answer is 76576500 (12375th triangle number, 576 factors)
