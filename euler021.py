cbba258fe7d7af0ecb86c047cfb858e9ae434701
Alex Eshoo <AlexEshoo@gmail.com>
2018-10-19 09:09:25 -0400

    formatting

--- euler021.py
+++ euler021.py
@@ -1,30 +1,30 @@
 from eulerTools import *
 
-def sumFactors(n):
-	f = divisors(n)
-	if n in f:
-		factors = f[2:]
-		return sum(factors) + 1
-	else:
-		return sum(f)
-	
-print sumFactors(220)
-print sumFactors(284)
+
+def sum_factors(k):
+    fact = divisors(k)
+    if k in fact:
+        factors = fact[2:]
+        return sum(factors) + 1
+    else:
+        return sum(fact)
+
+
+print(sum_factors(220))
+print(sum_factors(284))
 
 amic = []
 skip = []
-for n in range(2,10000):
-	if n in skip: continue
-	dn = sumFactors(n)
-	m = dn
-	if sumFactors(m) == n and n != m:
-		print "Found one", n, m
-		if m > n: skip.append(m)
-		amic.extend([n,m])
-		
+for n in range(2, 10000):
+    if n in skip:
+        continue
+    dn = sum_factors(n)
+    m = dn
+    if sum_factors(m) == n and n != m:
+        print("Found one", n, m)
+        if m > n:
+            skip.append(m)
+        amic.extend([n, m])
 
-print amic
-print sum(amic)
-	
-	
-
+print(amic)
+print(sum(amic))
