d63917f8aa9c96bec197aa7aaabc5a9ac6a54a55
Alex Eshoo <AlexEshoo@gmail.com>
2018-10-21 16:48:41 -0400

    formatting

--- euler031.py
+++ euler031.py
@@ -1,4 +1,3 @@
-import json
 target = 200
 denominations = [1, 2, 5, 10, 20, 50, 100, 200]
 ways = [0] * (target + 1)
@@ -7,6 +6,5 @@ ways[0] = 1
 for d in denominations:
     for j in range(d, target+1):
         ways[j] += ways[j - d]
-
 
 print("Answer:", ways[-1])
